6bd87431b14c7c30db5df52a22c01d475ae95f31
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-02-17 20:02:36 +0530

    adding routes roduct, getProductDetails

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getAllProducts , createProduct} from "../controller/productController.js";
+import { getAllProducts , createProduct, updateProduct, deleteProduct, getProductDetails} from "../controller/productController.js";
 
 const router=express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/product/new").post(getAllProducts);
+router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
 
 
 export default router;
