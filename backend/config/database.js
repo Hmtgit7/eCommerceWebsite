d2d804dc76cbbd6bc389139cc13b4ca264ca4f9f
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-02-17 20:00:20 +0530

    removing catch block

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,15 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = () => {
   mongoose
-    .connect(process.env.DB_URI, {
+    .connect("mongodb+srv://hmtloharcoding3579:hmtlohar123@cluster0.bhyqv8j.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      // useCreateIndex: true,
     })
     .then((data) => {
-      console.log(`Database connected: ${data.connection.host}`);
-    })
-    .catch((err) => {
-      console.log("Database connection error", err);
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
     });
 };
 
