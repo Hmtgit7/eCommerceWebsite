c1b8f773d7dd00179a51ab85fe9c0c7425563d03
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-02-17 20:00:45 +0530

    modify product model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,18 +3,19 @@ import mongoose from "mongoose";
 const productSchema = mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please Enter Product Name"],
+    required: [true, "Please Enter product Name"],
+    trim: true,
   },
   description: {
     type: String,
-    required: [true, "Please Enter Product Description"],
+    required: [true, "Please Enter product Description"],
   },
   price: {
     type: Number,
-    required: [true, "Please Enter Product Price"],
+    required: [true, "Please Enter product Price"],
     maxLength: [8, "Price cannot exceed 8 characters"],
   },
-  rating: {
+  ratings: {
     type: Number,
     default: 0,
   },
@@ -30,41 +31,41 @@ const productSchema = mongoose.Schema({
       },
     },
   ],
-  category:{
-    type:String,
-    required:[true, "Please Enter Product Category"]
+  category: {
+    type: String,
+    required: [true, "Please Enter Product Category"],
   },
-  Stock:{
-    type:Number,
-    required:[true, "Please Enter Product Stock"],
-    maxLength:[4,"Stock cannot exceed 4 characters"],
-    default:1,
+  Stock: {
+    type: Number,
+    required: [true, "Please Enter product Stock"],
+    maxLength: [4, "Stock cannot exceed 4 characters"],
+    default: 1,
   },
-  numberOfReviews:{
-    type:Number,
-    default:0,
+  numOfReviews: {
+    type: Number,
+    default: 0,
   },
-  reviews:[
+  reviews: [
     {
-        name:{
-            type:String,
-            required:true,
-        },
-        rating:{
-            type:Number,
-            required:true,
-        },
-        comment:{
-            type:String,
-            required:true,
-        }
-    }
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
   ],
-  createdAt:{
-    type:Date,
-    default:Date.now
-  }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-
-export default productSchema;
+const Product=mongoose.model("User", productSchema);
+export default Product;
