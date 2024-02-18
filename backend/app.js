4b544308e2db9a247677e9d442acf961c983bc6f
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-02-18 23:12:20 +0530

    configuring middleware to app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 import  express from "express";
 
+//middleware
+import { errorMiddleware } from "./middleware/error.js";
+
 const app = express();
 app.use(express.json());
 
@@ -8,5 +11,7 @@ import product from "./routes/productRoute.js";
 
 app.use("/api/v1", product);
 
+//MIDDLEWARE FOR ERROR
+app.use(errorMiddleware);
 
 export default app;
