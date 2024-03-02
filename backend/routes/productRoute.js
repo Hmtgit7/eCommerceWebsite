e001ca3a160a60f069cbc66b3e9cc20a79b30108
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-03-02 19:24:13 +0530

    add isAuthenticate method to all product routes whether user is permitted for making changes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,21 @@
-import express from "express"
-import { getAllProducts , createProduct, updateProduct, deleteProduct, getProductDetails} from "../controller/productController.js";
+import express from "express";
+import {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails,
+} from "../controller/productController.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middleware/auth.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.route("/products").get(getAllProducts);
-router.route("/product/new").post(createProduct);
-router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
+router.route("/products").get(isAuthenticatedUser,authorizeRoles("admin"),getAllProducts);
+router.route("/product/new").post(isAuthenticatedUser,createProduct);
+router
+  .route("/product/:id")
+  .put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct)
+  .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
+  .get(getProductDetails);
 
-
-export default router;
+export default router;   
