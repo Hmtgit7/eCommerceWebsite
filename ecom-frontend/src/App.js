a40ed4347ea8b64e907a5d3863fe5c72b5cdc431
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-08-24 17:29:14 +0530

    remaining data files

--- ecom-frontend/src/App.js
+++ ecom-frontend/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Navigation from "./Customer/Component/Navigation/Navigation";
 import HomePage from "./Customer/Pages/HomePage/HomePage";
-
+import Product from "./Customer/Component/Product/Product";
+import Footer from "./Customer/Component/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navigation />
       <div>
-        <HomePage />
+        {/* <HomePage /> */}
+        <Product />
       </div>
+      <Footer />
     </div>
   );
 };
