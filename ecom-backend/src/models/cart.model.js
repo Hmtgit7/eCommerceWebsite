4061d54e542f9c5d53e4e5aba3912cf47bfa36d7
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-09-04 09:59:34 +0530

    cart model

--- ecom-backend/src/models/cart.model.js
+++ ecom-backend/src/models/cart.model.js
@@ -9,7 +9,7 @@ const cartSchema = new mongoose.Schema({
   cartItems: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "cartItems",
-    required: true,
+    // required: true,
   },
   totalPrice: {
     type: Number,
