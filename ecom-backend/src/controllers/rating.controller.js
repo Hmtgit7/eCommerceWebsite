84601354c976ee8fb3bd061ca7d12f79261e1c0a
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-09-04 22:00:53 +0530

    rating controller

---
+++ ecom-backend/src/controllers/rating.controller.js
@@ -0,0 +1,26 @@
+const ratingService = require("../services/rating.service.js");
+
+const createRating = async (req, res) => {
+    const user = req.user;
+    try {
+        const rating = await ratingService.createRating(req.body, user);
+        return res.status(201).send(rating);
+    } catch (error) {
+        return res.status(500).send({ message: error.message });
+    }
+}
+
+const getAllRatings = async (req, res) => {
+    const productId = req.params.productId;
+    try {
+        const ratings = await ratingService.getProductRating(productId);
+        return res.status(200).send(ratings);
+    } catch (error) {
+        return res.status(500).send({ message: error.message });
+    }
+}
+
+module.exports = {
+    createRating,
+    getAllRatings
+}
