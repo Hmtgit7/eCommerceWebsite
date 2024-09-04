a55912bb676d9f6f4e702ec57110f9b14c82be7c
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-09-04 10:01:51 +0530

    adding routes to index file

--- ecom-backend/src/index.js
+++ ecom-backend/src/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/auth.route.js");
+const userRoutes = require("./routes/user.route.js");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  return res.status(200).send({
-    message: "Hello World",
-  });
-});
+app.use("/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
